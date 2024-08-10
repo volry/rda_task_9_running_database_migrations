c931b721fc9ebeff4813996706ed6ae37cb74ab8
volry <v.v.ryzhuk@gmail.com>
2024-08-10 11:15:00 +0300

    add changes

--- task.sql
+++ task.sql
@@ -1,22 +1,22 @@
---liquibase formatted sql
+# --liquibase formatted sql
 
---changeset mate.acamemy:1 labels:0.0.1
+# --changeset mate.acamemy:1 labels:0.0.1
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Countries;
+# --rollback DROP TABLE Countries;
 
---changeset mate.acamemy:2 labels:0.0.1
+# --changeset mate.acamemy:2 labels:0.0.1
 CREATE TABLE Products (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Products;
+# --rollback DROP TABLE Products;
 
---changeset mate.acamemy:3 labels:0.0.1
+# --changeset mate.acamemy:3 labels:0.0.1
 CREATE TABLE Warehouses (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
@@ -25,9 +25,9 @@ CREATE TABLE Warehouses (
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Warehouses;
+# --rollback DROP TABLE Warehouses;
 
---changeset mate.acamemy:4 labels:0.0.1
+# --changeset mate.acamemy:4 labels:0.0.1
 CREATE TABLE ProductInventory (
     ID INT,
     ProductID INT,
@@ -37,4 +37,15 @@ CREATE TABLE ProductInventory (
     FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ProductInventory;
+# --rollback DROP TABLE ProductInventory;
+
+
+# --changeset mate.academy:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255)
+);
+
+# -- rollback DROP TABLE Users;
